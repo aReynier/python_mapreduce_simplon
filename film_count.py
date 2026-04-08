b3b3e4f06e8d0e77d773876d35708eb3a274c0a5
Aurore Reynier <reynier.aurore@gmail.com>
2026-04-08 23:46:25 +0200

    refactor(film_count): substract useless code

--- film_count.py
+++ film_count.py
@@ -30,33 +30,3 @@ def reduce_fn(film_list_sorted):
 film_count()
 
 
-def genre_duration():
-    # sorted_film = shuffle_fn(map_fn())
-    # reduce_fn(sorted_film)
-    map_fn()
-
-def map_fn():
-    with open('logs_cinestream.csv', newline='') as file:
-        rows = csv.DictReader(file)
-        film_list = []
-        for row in rows:
-                film_list.append([row['genre'], row['duree_visionnee_sec']])
-    return film_list
-
-def shuffle_fn(film_list):
-    film_list.sort()
-    print(film_list)
-    return film_list
-
-# def reduce_fn(film_list_sorted):
-#     film_list_sorted
-#     unique_films_list = list(set(film_list_sorted))
-#     films_count = []
-#     for unique_film_list in unique_films_list:
-#          count = film_list_sorted.count(unique_film_list)
-#          films_count.append([unique_film_list, count])
-#     print(films_count)
-
-genre_duration()
-
-
