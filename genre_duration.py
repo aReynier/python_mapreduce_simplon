4cf9a033f63705ebb596f83967555079b903c075
Aurore Reynier <reynier.aurore@gmail.com>
2026-04-09 01:04:22 +0200

    refactor(genre_duration): Apply better mapreduce logic

--- genre_duration.py
+++ genre_duration.py
@@ -1,42 +1,45 @@
 import csv
-logs = 'logs_cinestream.csv'
 
 
 # The goal of map reduce is to:
 # have total view lenght per genre
 def genre_duration():
-    sorted_film = shuffle_fn(map_fn())
-    reduce_fn(sorted_film)
+    main()
 
-def map_fn():
-    with open('logs_cinestream.csv', newline='') as file:
+def main():
+    logs = 'logs_cinestream.csv'
+
+    with open(logs, newline='') as file:
         rows = csv.DictReader(file)
         film_list = []
         for row in rows:
-            film_list.append({'genre':row['genre'], 'duree_visionnee_sec':row['duree_visionnee_sec']})
-    return film_list
-
-def shuffle_fn(film_list):
-    film_duration_sorted = sorted(film_list, key=lambda d: int(d['duree_visionnee_sec']))
-    film_genre_sorted = sorted(film_duration_sorted, key=lambda d: d['genre'])
-    return film_genre_sorted
-
-def reduce_fn(films_list_sorted):
-    genres = []
-    for film_list_sorted in films_list_sorted:
-        genres.append(film_list_sorted['genre'])
-    unique_genres = list(set(genres))
-    print(unique_genres)
-    duration_per_genre = []
-    for unique_genre in unique_genres:
-        duration = 0
-        for film_list_sorted in films_list_sorted:
-            if (film_list_sorted['genre'] == unique_genre):
-                duration += int(film_list_sorted['duree_visionnee_sec'])
-
-        duration_per_genre.append([unique_genre, duration])
-    print(duration_per_genre)
+                film_list.append(map_fn(row))
+    sorted_data = shuffle_fn(film_list)
     
+    reduce_fn(sorted_data)
+
+
+def map_fn(data):
+    mapped_films = (data['genre'], int(data['duree_visionnee_sec']))
+
+    return mapped_films
+
+
+def shuffle_fn(data):
+    shuffled_films = {}
+
+    for value in data:
+        if value[0] not in shuffled_films:
+             shuffled_films[value[0]] = []
+        shuffled_films[value[0]].append(value[1])
+    return shuffled_films
+
+
+def reduce_fn(data):
+    for value in data:
+        data[value] = sum(data[value])
+    print(data)
+
 
 genre_duration()
 
