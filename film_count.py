5462ff8b9ca4e824dda3ec787c6f5fab783377d5
Aurore Reynier <reynier.aurore@gmail.com>
2026-04-09 00:59:25 +0200

    refactor(film_count): Apply better mapreduce logic

--- film_count.py
+++ film_count.py
@@ -1,32 +1,42 @@
 import csv
-logs = 'logs_cinestream.csv'
-
 
 # The goal of map reduce is to:
 # have total view number per movie
 def film_count():
-    sorted_film = shuffle_fn(map_fn())
-    reduce_fn(sorted_film)
+    main()
 
-def map_fn():
-    with open('logs_cinestream.csv', newline='') as file:
+def main():
+    logs = 'logs_cinestream.csv'
+
+    with open(logs, newline='') as file:
         rows = csv.DictReader(file)
         film_list = []
         for row in rows:
-                film_list.append(row['film_title'])
-    return film_list
+                film_list.append(map_fn(row))
+    sorted_data = shuffle_fn(film_list)
+    
+    reduce_fn(sorted_data)
 
-def shuffle_fn(film_list):
-    film_list.sort()
-    return film_list
 
-def reduce_fn(film_list_sorted):
-    unique_films_list = list(set(film_list_sorted))
-    films_count = []
-    for unique_film_list in unique_films_list:
-         count = film_list_sorted.count(unique_film_list)
-         films_count.append([unique_film_list, count])
-    print(films_count)
+def map_fn(data):
+    mapped_films = (data["film_title"], 1)
+
+    return mapped_films
+
+
+def shuffle_fn(data):
+    shuffled_films = {}
+
+    for value in data:
+        if value[0] not in shuffled_films:
+             shuffled_films[value[0]] = []
+        shuffled_films[value[0]].append(value[1])
+
+    return shuffled_films
+
+
+def reduce_fn(data):
+    for value in data:
+        data[value] = sum(data[value])
+    print(data)
 film_count()
-
-
